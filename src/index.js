73685d42786ef560d6bd39b2fe4700877517f99b
GabiProg <gabrielaterese07@gmail.com>
2022-09-16 16:37:25 -0300

    test

--- src/index.js
+++ src/index.js
@@ -14,6 +14,6 @@ app.use(cors());
 app.use(authRouter);
 app.use(homeRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; //testing deploy
 
 app.listen(PORT, () => console.log('The server is listening on ' + PORT + '.'));
